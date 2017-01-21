6fd9da29247d9d35a126cc9741cf92868ac60b5a
MrSherlok <antomike@mail.ru>
2017-01-21 14:14:34 +0200

    new scene

---
+++ Assets/Scripts/CameraFollowScript2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript2 : MonoBehaviour {
+	private Vector2 velocity;
+
+	public float camLevelChangeSmooth;
+
+
+	GameObject player;
+
+	void Start () {
+		player =  GameObject.Find("Komarik");
+	}
+	void FixedUpdate () {
+        gameObject.transform.position = Vector3.Lerp(new Vector3(player.transform.position.x, transform.position.y, transform.position.z), new Vector3(0f, player.transform.position.y+4f, transform.position.z), camLevelChangeSmooth);
+	}
+}
---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour
+{	
+
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 direction = new Vector2(-1, 0);
+
+    	
+	void FixedUpdate()
+	{	
+		// Movement
+		Vector3 movement = new Vector3(
+			speed.x * direction.x,
+			speed.y * direction.y,
+			0);
+		
+		movement *= Time.deltaTime;
+		transform.Translate(movement);
+	}
+}
